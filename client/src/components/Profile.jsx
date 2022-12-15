76913bbe622f567bcf7da2fcfcf0e6668d517bd0
Alaysia Veal <alaysiaveal@gmail.com>
2022-12-15 00:10:00 -0500

    added base form for editing username/password

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,7 +1,46 @@
+import { useNavigate } from "react-router-dom"
+import Client from "../services/api"
+import { useState } from "react"
+
+
+
 const Profile = () => {
+  const navigate = useNavigate()
+  const initial = {
+    username: '',
+    password: '',
+    confirmPassword: ''
+  }
+  const [form, setForm] = useState(initial)
+
+  const [edit, setEdit] = useState(initial)
+  
+  const handleEdit = (e) => {
+    setEdit({ ...edit, [e.target.name]: e.target.value })
+  }
+  const handleEditSubmit = async (e, id) => {
+    /* const editButtonRef = editButton.current
+    const textarea = ref.current
+    const submitEdit = submitEditRef.current
+    textarea.readOnly = true
+    textarea.className = 'reviewArea'
+    submitEdit.className = 'hiddenButton'
+    editButtonRef.className = '' */
+    e.preventDefault()
+    await Client.put(`/user/${id}`)
+    navigate('/profile')
+  }
   return (
     <div>
       <h2>edit profile</h2>
+      <form>
+        <label>username</label>
+        <input/>
+        <label>password</label>
+        <input/>
+        <label>confirm password</label>
+        <input></input>
+      </form>
     </div>
   )
 }
